f6b11d2d1a68127a852769572bbfd7d90f529834
swj <159632_@naver.com>
2022-02-16 05:47:20 +0900

    [test]mirror repo 테스트

--- src/main/java/guru/springframework/spring5webapp/repository/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repository/AuthorRepository.java
@@ -4,5 +4,4 @@ import guru.springframework.spring5webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-
 }
